ae7fad7de27218c5e6c0aab9c8f69694008c4e03
James Daley <jdaley@dialsource.com>
2017-01-29 17:56:57 -0800

    added R getting closer

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,12 @@
 import random
 
-qdict = {'x': {}, 'o': {}}
+#qdict[(board, piece)][(row,column)] = score
+#qdict[piece][board][(row,column)] = score
+#board = ((),(),())
+qdict = {'x':{}, 'o':{}}
+
 #qdict = {'x': [[ ]], 'o': {}}
 #qdict['x'][0][0][(board)]
-#qdict[piece][board][row][column]
 # the qdict represents a 4 dimensional coordinate which returns a score
 # if that score doesn't exist the object is initialized
 xwins = False
@@ -50,10 +53,33 @@ def nextMove(board, piece):
                 if score == 100:
                     xwins = piece == 'x'
                     owins = piece == 'o'
-                    print xwins, owins
                 moves.append((applyMove(r, c, piece, board), score))
-    print moves
+    # each move is (board, score)
     return moves
+
+def R(state, r, c, piece):
+    board = applyMove(r, c, piece, state)
+    score = 0
+    # set reward for win states
+    # horizontal
+    if board[r][(c - 1) % 3] == piece and board[r][(c + 1) % 3] == piece:
+        score = 100
+    # vertical
+    if board[(r - 1) % 3][c] == piece and board[(r + 1) % 3][c] == piece:
+        score = 100
+    # diagonal to left
+    if r == c and board[(r - 1) % 3][(c - 1) % 3] == piece and board[(r + 1) % 3][(c + 1) % 3] == piece:
+        score = 100
+    # diagonal to right
+    if (r + 2) % 2 == (c - 2) % 2 and board[(r - 1) % 3][(c + 1) % 3] == piece and board[(r + 1) % 3][(c - 1) % 3] == piece:
+        score = 100
+    return score
+
+# you need next move q matrix
+
+# Q(state, action) = R(state, action) + (gamma * max(all possible q scores from statePrime))
+# statePrime is the state after action is applied
+# all possible q scores is the list of all actions from statePrime
 
 def getmax(moveslist):
     best = max(moveslist, key=lambda x: x[1])
@@ -67,12 +93,22 @@ initial = ((('', '', ''),('', '', ''),('', '', '')), 0)
 # an episode is a game
 def getNextBoardScore(boardScore, piece, printBool):
         board, score = boardScore
-        nextBoardScore = getmax(nextMove(board, 'x'))
+        nextBoardScore = getmax(nextMove(board, piece))
         nextBoard, nextScore = nextBoardScore
         qdict[piece][board] = score + (gamma * (nextScore - score))
         # print for debug
         if printBool:
-            print piece
+            print 'piece', piece
+            printBoard(nextBoard)
+        return nextBoardScore
+
+def makeMove(board, piece, printBool):
+        nextBoardScore = getmax(nextMove(board, piece))
+        nextBoard, nextScore = nextBoardScore
+        qdict[piece][board] = score + (gamma * (nextScore - score))
+        # print for debug
+        if printBool:
+            print 'piece', piece
             printBoard(nextBoard)
         return nextBoardScore
 
@@ -83,11 +119,12 @@ def episode(printBool):
     owins = False
     # update matrix values
     while not xwins and not owins:
-        xboardScore = getNextBoardScore(xboardScore, 'x', printBool)
+        board = makeMove(board, 'x', printBool)
+        # I want to pass the board between the two players
         if xwins:
             xwins = False
             break
-        oboardScore = getNextBoardScore(oboardScore, 'o', printBool)
+        board = makeMove(board, 'o', printBool)
         if owins:
             owins = False
             break
