e21e9fd364771a6b7a4ada6f3ff3b2d5fa2e51e1
James Daley <jdaley@dialsource.com>
2017-01-30 21:28:29 -0800

    incomplete

--- tictactoe.py
+++ tictactoe.py
@@ -30,32 +30,27 @@ def printBoard(board):
     print board[1]
     print board[2]
 
-# R matrix
-def nextMove(board, piece):
+def possibleMoves(board):
     moves = []
     for r, row in enumerate(board):
         for c, cell in enumerate(row):
-            score = 0
             if cell is '':
-                # set reward for win states
-                # horizontal
-                if board[r][(c - 1) % 3] == piece and board[r][(c + 1) % 3] == piece:
-                    score = 100
-                # vertical
-                if board[(r - 1) % 3][c] == piece and board[(r + 1) % 3][c] == piece:
-                    score = 100
-                # diagonal to left
-                if r == c and board[(r - 1) % 3][(c - 1) % 3] == piece and board[(r + 1) % 3][(c + 1) % 3] == piece:
-                    score = 100
-                # diagonal to right
-                if (r + 2) % 2 == (c - 2) % 2 and board[(r - 1) % 3][(c + 1) % 3] == piece and board[(r + 1) % 3][(c - 1) % 3] == piece:
-                    score = 100
-                if score == 100:
-                    xwins = piece == 'x'
-                    owins = piece == 'o'
-                moves.append((applyMove(r, c, piece, board), score))
-    # each move is (board, score)
+                moves.append((r, c))
     return moves
+
+def qSet(piece, board, action, score):
+    if not (board in qdict[piece]):
+        qdict[piece] = {board: {action: score}}
+    else:
+        qdict[piece][board][action] = score
+
+def qGet(piece, board, action):
+    if not (board in qdict[piece]):
+        return 0
+    if not (action in qdict[piece][board]):
+        return 0
+    else:
+        return qdict[piece][board][action]
 
 def R(state, r, c, piece):
     board = applyMove(r, c, piece, state)
@@ -81,40 +76,65 @@ def R(state, r, c, piece):
 # statePrime is the state after action is applied
 # all possible q scores is the list of all actions from statePrime
 
-def getmax(moveslist):
-    best = max(moveslist, key=lambda x: x[1])
+def getMax(moveScores, piece):
+    best = max(moveScores, key=lambda x: x[1])
     if best[1] == 0:
         # if they're all 0 then randomly pick one
-        best = random.choice(moveslist)
+        best = random.choice(moveScores)
     return best
 
 gamma = 0.8
-initial = ((('', '', ''),('', '', ''),('', '', '')), 0)
-# an episode is a game
-def getNextBoardScore(boardScore, piece, printBool):
-        board, score = boardScore
-        nextBoardScore = getmax(nextMove(board, piece))
-        nextBoard, nextScore = nextBoardScore
-        qdict[piece][board] = score + (gamma * (nextScore - score))
-        # print for debug
-        if printBool:
-            print 'piece', piece
-            printBoard(nextBoard)
-        return nextBoardScore
+GAMMA = gamma
+
+def getMovesQScores(board, moves, piece):
+    moveScores = []
+    for move in moves:
+        moveScores.append((move, qGet(piece, board, move)))
+    return moveScores
+
+def getMovesRScores(board, moves, piece):
+    moveScores = []
+    for move in moves:
+        moveScores.append((move, R(board, move[0], move[1], piece)))
+    return moveScores
+
+def evaluate(nboard, piece):
+    # what this should do is randomly place opposing piece everywhere
+    # then evaluate next best move
+    opponent = 'o' if piece is 'x' else 'x'
+    opponentMoves = possibleMoves(nboard)
+    boardPrimes = []
+    for move in opponentMoves:
+        r = move[0]
+        c = move[1]
+        oboard = applyMove(r, c, opponent, nboard)
+        moveScores = getMovesQScores(oboard, possibleMoves(oboard), piece)
+        moveScore = getMax(moveScores, piece)
+    # is highest possible score
+    return moveScore[1]
 
 def makeMove(board, piece, printBool):
-        nextBoardScore = getmax(nextMove(board, piece))
-        nextBoard, nextScore = nextBoardScore
-        qdict[piece][board] = score + (gamma * (nextScore - score))
+        moves = possibleMoves(board)
+        moveScores = getMovesQScores(board, moves, piece)
+        moveScore = getMax(moveScores, piece)
+        # move is best move from current board
+        move = moveScore[0]
+        qscore = moveScore[1]
+        # asdfasdf
+        nboard = applyMove(move[0], move[1], piece, board)
+        nqscore = evaluate(nboard, piece)
+        # move is best move from current board
+        score = R(board, move[0], move[1], piece) + (GAMMA * nqscore)
+        qSet(piece, board, action, score)
         # print for debug
         if printBool:
             print 'piece', piece
             printBoard(nextBoard)
         return nextBoardScore
 
+# an episode is a game
 def episode(printBool):
-    xboardScore = ((('', '', ''),('', '', ''),('', '', '')), 0)
-    oboardScore = ((('', '', ''),('', '', ''),('', '', '')), 0)
+    board = (('', '', ''),('', '', ''),('', '', ''))
     xwins = False
     owins = False
     # update matrix values
